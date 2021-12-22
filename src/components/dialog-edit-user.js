ba936fe0350c6fd6ff6370291c9e36c30f0134bd
Shosuke, Momotani <Shosuke.Momotani@sony.com>
2021-12-22 20:21:27 +0900

    slider input for goal score

--- src/components/dialog-edit-user.js
+++ src/components/dialog-edit-user.js
@@ -12,6 +12,7 @@ import {
   FormGroup,
   FormControlLabel,
   Checkbox,
+  Slider,
 } from "@mui/material";
 
 import Player from "../player";
@@ -29,10 +30,20 @@ export default function DialogEditUser({ player = new Player(""), onClose, open 
 
   const [name, setName] = React.useState(player.name);
   const [score, setScore] = React.useState(player.score);
-  const [scoreMax, setScoreMax] = React.useState(player.scoreMax);
-  const [scoreRestart, setScoreRestart] = React.useState(player.scoreRestart);
   const [disqualification, setDisqualification] = React.useState(player.disqualification);
   const [error, setError] = React.useState(player.error);
+  const [scoreSettings, setScoreSettings] = React.useState([player.scoreRestart, player.scoreMax]);
+
+  const handleChangeScoreSettings = (event, newValues, activeThumb) => {
+    if (!Array.isArray(newValues)) {
+      return;
+    }
+    if (activeThumb === 0) {
+      setScoreSettings([Math.min(newValues[0], scoreSettings[1] - 5), scoreSettings[1]]);
+    } else {
+      setScoreSettings([scoreSettings[0], Math.max(newValues[1], scoreSettings[0] + 5)]);
+    }
+  };
 
   const handleClose = (update, reason) => {
     // diable backdrop escape
@@ -40,22 +51,27 @@ export default function DialogEditUser({ player = new Player(""), onClose, open 
 
     onClose(
       update
-        ? new Player(name, { score, scoreMax, scoreRestart, disqualification, error: disqualification ? error : 0 })
+        ? new Player(name, {
+            score,
+            scoreMax: scoreSettings[1],
+            scoreRestart: scoreSettings[0],
+            disqualification,
+            error: disqualification ? error : 0,
+          })
         : null
     );
 
     setName("");
     setScore(0);
-    setScoreMax(50);
-    setScoreRestart(25);
     setError(0);
+    setScoreSettings([25, 50]);
   };
 
   return (
     <Dialog onClose={handleClose} open={open} className={classes.root}>
       <DialogTitle>プレーヤー設定</DialogTitle>
       <DialogContent dividers>
-        <Grid container rowSpacing={3}>
+        <Grid container rowSpacing={3} columnSpacing={2} alignItems="center">
           <Grid item xs={12}>
             <TextField
               autoFocus
@@ -69,14 +85,14 @@ export default function DialogEditUser({ player = new Player(""), onClose, open 
               }}
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={6}>
             <TextField
               id="score"
               variant="standard"
               label="得点"
               fullWidth
               type="number"
-              inputProps={{ max: scoreMax, min: 0, step: 1 }}
+              inputProps={{ max: scoreSettings[1], min: 0, step: 1 }}
               value={String(score)}
               InputLabelProps={{
                 shrink: true,
@@ -86,56 +102,7 @@ export default function DialogEditUser({ player = new Player(""), onClose, open 
               }}
             />
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="score-max"
-              variant="standard"
-              label="ゴールの点数"
-              fullWidth
-              type="number"
-              inputProps={{ max: 100, min: 0, step: 5 }}
-              value={String(scoreMax)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onChange={(e) => {
-                setScoreMax(Number(e.target.value));
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="score-restart"
-              variant="standard"
-              label="ドボン時の点数"
-              fullWidth
-              type="number"
-              inputProps={{ max: scoreMax - 5, min: 0, step: 5 }}
-              value={String(scoreRestart)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onChange={(e) => {
-                setScoreRestart(Number(e.target.value));
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={disqualification}
-                    onChange={(event) => {
-                      setDisqualification(event.target.checked);
-                    }}
-                  />
-                }
-                label="連続失敗で失格にする"
-              />
-            </FormGroup>
-          </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={6}>
             <TextField
               id="error"
               variant="standard"
@@ -152,6 +119,36 @@ export default function DialogEditUser({ player = new Player(""), onClose, open 
                 setError(Number(e.target.value));
               }}
             />
+          </Grid>
+          <Grid item xs={2}>
+            ゴール設定
+          </Grid>
+          <Grid item xs={9} style={{ marginTop: 20 }}>
+            <Slider
+              min={0}
+              max={80}
+              step={5}
+              value={scoreSettings}
+              onChange={handleChangeScoreSettings}
+              valueLabelDisplay="on"
+              disableSwap
+              valueLabelFormat={(value, index) => (index === 0 ? `ドボン時：${value}` : `ゴール：${value}`)}
+            ></Slider>
+          </Grid>
+          <Grid item xs={12}>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={disqualification}
+                    onChange={(event) => {
+                      setDisqualification(event.target.checked);
+                    }}
+                  />
+                }
+                label="連続失敗で失格にする"
+              />
+            </FormGroup>
           </Grid>
         </Grid>
       </DialogContent>
